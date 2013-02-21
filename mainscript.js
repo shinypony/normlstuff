ba92f2abe0889c50d25e26409f192e674d17199c
Gunther Krause <gunther@gunther-Inspiron-M5030.(none)>
2013-02-20 20:21:11 -0600

    changed div and html file

--- mainscript.js
+++ mainscript.js
@@ -20,7 +20,7 @@ function include(filename, onload) {
 include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
     $(document).ready(function() {
         $.getScript("innoculation.js");
-		$.getScript("emotes.js");
-		$("#content1").load('features.html');;
+        $.getScript("emotes.js");
+        $("#description").load('description.html');;
     });
 });
